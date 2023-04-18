934f05d8032cb1b12ae570a4152cd04da2a693c7
appletyler <apple_tyler@mail.ru>
2023-04-18 22:51:00 +0300

    mult

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,56 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       int x = 20;
-       while (x>=1){
-           System.out.println(x);
-           x-=2;
-      }
-        System.out.println("--------------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        While While = new While();
+        System.out.println("введите первое число: ");
+        int i = scanner.nextInt();
+        System.out.print("введите второе число: ");
+        int j = scanner.nextInt();
+        int result;
 
-       for(int i = 1; i < 10; i++) {
-            for(int j = 1; j < 10; j++) {
-                System.out.print(i * j + " ");
-            }
-            System.out.println("");
-        }
-        System.out.println("--------------------------------------------");
 
-        for(int i = 1; i < 10; i++) {
-           for(int j = 1; j < 10; j++) {
-               System.out.print(i + j + " ");
-        }
-          System.out.println("");
-      }
-        System.out.println("--------------------------------------------");
 
-      for (int i =0; i<=34; i++){
-         for (int j =0; j<=34; j++){
-           System.out.println(i + j + " ");
-     }
-     System.out.println("");
-  }
-        System.out.println("--------------------------------------------");
-        for(int i = 0; i <= 34; i++) { // инициализация, условие, инкремент
-            for (int j = 0; j <= 34; j++)
-                if (i % 3 == 0) {
-                    System.out.println(i);
-                    i++;
-                }
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int i = scanner.nextInt();
+//       int x = 20;
+//       while (x>=1){
+//           System.out.println(x);
+//           x-=2;
+//      }
+//        System.out.println("--------------------------------------------");
+
+
+
+ //       for(int i = 1; i < 10; i++) {
+ //          for(int j = 1; j < 10; j++) {
+  //             System.out.print(i + j + " ");
+   //     }
+  //        System.out.println("");
+  //    }
+  //      System.out.println("--------------------------------------------");
+
+ //     for (int i =0; i<=34; i++){
+ //        for (int j =0; j<=34; j++){
+  //         System.out.println(i + j + " ");
+ //    }
+  //   System.out.println("");
+ // }
+
 
     }
 }
--- src/While.java
+++ src/While.java
@@ -1,2 +1,13 @@
 public class While {
+
+    public int multiply (int i, int j) {
+        for(i = 1; i <= 10; i++) {
+            for (j = 1; j <= 10; j++)
+                System.out.print(i * j);
+
+        }
+
+        return i*j;
+    }
+
 }
